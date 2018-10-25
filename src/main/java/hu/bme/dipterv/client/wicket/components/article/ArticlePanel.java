6eb2ef4c7858d2a3d54d4c55ca7f450876646ef0
zol1e <zoltan.szeg@gmail.com>
2018-10-25 22:30:40 +0200

    Cikkek kilistázása wicket kliensen
    Spring article service-t egy @SpringBean annotácóval beinjektáltam a wicketes article panelbe.
    A listview megjelenítőnek ezt a serivce-t kell adni, mint propertymodel és a "findAll" kell mint az összes elem lekérdezése a service-től.

--- src/main/java/hu/bme/dipterv/client/wicket/components/article/ArticlePanel.java
+++ src/main/java/hu/bme/dipterv/client/wicket/components/article/ArticlePanel.java
@@ -1,17 +1,44 @@
 package hu.bme.dipterv.client.wicket.components.article;
 
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.PageableListView;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.springframework.security.authentication.AuthenticationManager;
+
+import hu.bme.dipterv.domain.Article;
+import hu.bme.dipterv.service.ArticleService;
+import hu.bme.dipterv.service.dto.ArticleDTO;
 
 public class ArticlePanel extends Panel {
 	
+	@SpringBean
+    private ArticleService articleService;
+	
+	@SuppressWarnings("serial")
 	public ArticlePanel(String id) {
 		super(id);
 		
-		Model<String> welcomeModel = Model.of("Welcome on article page");
+		Model<String> welcomeModel = Model.of("Articles");
 		Label label = new Label("lblArticlePage", welcomeModel);
 		add(label);
+		
+        final PageableListView<ArticleDTO> listView;
+        add(listView = new PageableListView<ArticleDTO>("articles", new PropertyModel<>(articleService,
+            "findAll"), 4)
+        {
+            @Override
+            public void populateItem(final ListItem<ArticleDTO> listItem)
+            {
+                final ArticleDTO article = listItem.getModelObject();
+                listItem.add(new Label("title", new Model<>(article.getTitle())));
+                listItem.add(new Label("author", new Model<>(article.getWriterId())));
+                listItem.add(new Label("content", new Model<>(article.getContent())));
+            }
+        });
 	}
 
 }
